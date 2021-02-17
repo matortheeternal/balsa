ca6fa32239a3fc52a44e8775550e670915eb4128
matortheeternal <why101@msn.com>
2021-02-17 15:44:28 -0800

    added helpers to shared FileSource class

--- balsa/shared/FileSource.cs
+++ balsa/shared/FileSource.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.IO.MemoryMappedFiles;
+using System.Text;
 
 namespace balsa.shared {
     public class FileSource {
@@ -11,6 +13,7 @@ namespace balsa.shared {
         private readonly FileInfo fileInfo;
 
         internal long fileSize => fileInfo.Length;
+        internal virtual Encoding stringEncoding => throw new NotImplementedException();
 
         internal FileSource(string filePath) {
             this.filePath = filePath;
@@ -27,6 +30,48 @@ namespace balsa.shared {
                 0, 0, MemoryMappedFileAccess.Read
             );
             reader = new BinaryReader(stream);
+        }
+
+        internal dynamic ReadPrefix(int prefixLength) {
+            switch (prefixLength) {
+                case 1: return reader.ReadByte();
+                case 2: return reader.ReadUInt16();
+                case 4: return reader.ReadUInt32();
+            }
+            throw new Exception($"Unknown prefix length {prefixLength}");
+        }
+
+        internal string ReadZString(byte terminator = 0) {
+            byte[] bytes = new byte[32];
+            int i = 0;
+            do {
+                byte b = reader.ReadByte();
+                if (b == 0) break;
+                if (i >= bytes.Length) {
+                    var newBytes = new byte[bytes.Length * 2];
+                    bytes.CopyTo(newBytes, 0);
+                    bytes = newBytes;
+                }
+                bytes[i++] = b;
+            } while (true);
+            if (i == 0) return string.Empty;
+            return stringEncoding.GetString(bytes, 0, i);
+        }
+
+        internal string ReadBString(int len) {
+            var bytes = reader.ReadBytes(len);
+            return stringEncoding.GetString(bytes, 0, len);
+        }
+
+        internal string ReadString(int prefixLength = 0, bool nullTerminated = true) {
+            if (prefixLength > 0) {
+                dynamic len = ReadPrefix(prefixLength);
+                if (nullTerminated) len--;
+                return ReadBString(len);
+            }
+            if (!nullTerminated)
+                throw new Exception("Cannot read unterminated string of unknown length.");
+            return ReadZString();
         }
     }
 }
