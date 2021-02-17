c2f55b00603895897af6155a3150e07bbcfc574d
matortheeternal <why101@msn.com>
2021-02-17 15:43:51 -0800

    removed unneeded ArchiveFileSource methods

--- balsa/archives/ArchiveFileSource.cs
+++ balsa/archives/ArchiveFileSource.cs
@@ -8,54 +8,12 @@ namespace balsa.archives {
     public class ArchiveFileSource : FileSource {
         internal readonly ArchiveFile archive;
 
-        internal Encoding stringEncoding => archive.encoding;
+        internal override Encoding stringEncoding => archive.encoding;
 
         internal ArchiveFileSource(string filePath, ArchiveFile archive) :
             base(filePath) {
             this.archive = archive;
             archive.source = this;
-        }
-
-        internal dynamic ReadPrefix(int prefixLength) {
-            switch (prefixLength) {
-                case 1: return reader.ReadByte();
-                case 2: return reader.ReadUInt16();
-                case 4: return reader.ReadUInt32();
-            }
-            throw new Exception($"Unknown prefix length {prefixLength}");
-        }
-
-        internal string ReadZString(byte terminator = 0) {
-            byte[] bytes = new byte[32];
-            int i = 0;
-            do {
-                byte b = reader.ReadByte();
-                if (b == 0) break;
-                if (i >= bytes.Length) {
-                    var newBytes = new byte[bytes.Length * 2];
-                    bytes.CopyTo(newBytes, 0);
-                    bytes = newBytes;
-                }
-                bytes[i++] = b;
-            } while (true);
-            if (i == 0) return string.Empty;
-            return stringEncoding.GetString(bytes, 0, i);
-        }
-
-        internal string ReadBString(int len) {
-            var bytes = reader.ReadBytes(len);
-            return stringEncoding.GetString(bytes, 0, len);
-        }
-
-        internal string ReadString(int prefixLength = 0, bool nullTerminated = true) {
-            if (prefixLength > 0) {
-                dynamic len = ReadPrefix(prefixLength);
-                if (nullTerminated) len--;
-                return ReadBString(len);
-            }
-            if (!nullTerminated)
-                throw new Exception("Cannot read unterminated string of unknown length.");
-            return ReadZString();
         }
 
         internal byte[] Decompress(int dataSize) {
