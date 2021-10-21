51bf8e80fdeb9f534c04cbfe41d42b5a999b3529
Mator <why101@msn.com>
2021-10-21 15:18:27 -0700

    added basic TES5 stringtable file tests

---
+++ Tests/stringtables/TES5StringFileTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using balsa.setup;
+using balsa.stringtables;
+using balsa;
+using System.Collections.Generic;
+using System;
+
+namespace Tests.stringtables {
+    public class TES5StringFileTests {
+        public AssetManager manager;
+        public TES5StringFile stringFile;
+
+        [OneTimeSetUp]
+        public void Setup() {
+            manager = new AssetManager(Games.TES5);
+            var stringsPath = TestHelpers.FixturePath("TES5.strings");
+            stringFile = (TES5StringFile)manager.LoadStrings(stringsPath);
+        }
+
+        [Test]
+        public void TestStrings() {
+            var expectedStrings = new Dictionary<UInt32, string>(5) {
+                { 0x1, "Test" },
+                { 0x2, "Abc123" },
+                { 0x3, "Pasta" },
+                { 0x4, "Iron Thing" },
+                { 0xFF600, "One Very Long String" }
+            };
+            CollectionAssert.AreEqual(expectedStrings, stringFile.strings);
+        }
+    }
+}
