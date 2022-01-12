33f29cd7722cc750297ebe43978746ff1ecd426f
Mator <why101@msn.com>
2022-01-11 18:11:17 -0800

    add GetMatchingFiles and EnumerateEntries to FileContainer

---
+++ balsa/shared/FileContainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace balsa.shared {
+    public class FileContainer {
+        public virtual string path => throw new NotImplementedException();
+
+        public virtual List<string> GetFiles() {
+            throw new NotImplementedException();
+        }
+
+        public virtual List<string> GetMatchingFiles(Regex expr) {
+            return GetFiles().FindAll(filePath => {
+                return expr.IsMatch(filePath);
+            });
+        }
+
+        public virtual List<string> EnumerateEntries(string subPath) {
+            throw new NotImplementedException();
+        }
+    }
+}
